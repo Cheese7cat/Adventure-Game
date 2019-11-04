59f8c1d2b23e10b32899da9def6e39e864e114f3
Chees <Chees@LAPTOP-4SFMIA3G>
2019-11-04 12:59:36 -0500

    Add project files.

---
+++ AdventureGame/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace AdventureGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.output = new System.Windows.Forms.Label();
+            this.redOutput = new System.Windows.Forms.Label();
+            this.blueOutput = new System.Windows.Forms.Label();
+            this.back = new System.Windows.Forms.Label();
+            this.sceneImage = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sceneImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // output
+            // 
+            this.output.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.output.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(245, 274);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(279, 147);
+            this.output.TabIndex = 0;
+            // 
+            // redOutput
+            // 
+            this.redOutput.Location = new System.Drawing.Point(134, 279);
+            this.redOutput.Name = "redOutput";
+            this.redOutput.Size = new System.Drawing.Size(95, 62);
+            this.redOutput.TabIndex = 5;
+            // 
+            // blueOutput
+            // 
+            this.blueOutput.Location = new System.Drawing.Point(134, 357);
+            this.blueOutput.Name = "blueOutput";
+            this.blueOutput.Size = new System.Drawing.Size(95, 69);
+            this.blueOutput.TabIndex = 6;
+            // 
+            // back
+            // 
+            this.back.BackColor = System.Drawing.Color.Gray;
+            this.back.Location = new System.Drawing.Point(229, 250);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(305, 191);
+            this.back.TabIndex = 9;
+            // 
+            // sceneImage
+            // 
+            this.sceneImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.sceneImage.Location = new System.Drawing.Point(107, 36);
+            this.sceneImage.Name = "sceneImage";
+            this.sceneImage.Size = new System.Drawing.Size(357, 211);
+            this.sceneImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.sceneImage.TabIndex = 10;
+            this.sceneImage.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AdventureGame.Properties.Resources.redButton;
+            this.pictureBox1.Location = new System.Drawing.Point(28, 279);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::AdventureGame.Properties.Resources.blueButton;
+            this.pictureBox2.Location = new System.Drawing.Point(28, 357);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(63, 69);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(229, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 191);
+            this.label1.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(229, 441);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(305, 10);
+            this.label2.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.DimGray;
+            this.label3.Location = new System.Drawing.Point(530, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 201);
+            this.label3.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.DimGray;
+            this.label4.Location = new System.Drawing.Point(229, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(311, 12);
+            this.label4.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.DimGray;
+            this.label5.Location = new System.Drawing.Point(457, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 227);
+            this.label5.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.DimGray;
+            this.label6.Location = new System.Drawing.Point(91, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(10, 227);
+            this.label6.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.DimGray;
+            this.label7.Location = new System.Drawing.Point(94, 250);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(373, 10);
+            this.label7.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.DimGray;
+            this.label8.Location = new System.Drawing.Point(94, 23);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(373, 10);
+            this.label8.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.DimGray;
+            this.label9.Location = new System.Drawing.Point(91, 250);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 10);
+            this.label9.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.Gray;
+            this.label10.Location = new System.Drawing.Point(100, 33);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(367, 217);
+            this.label10.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(550, 464);
+            this.Controls.Add(this.sceneImage);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.redOutput);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.blueOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.sceneImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label output;
+        private System.Windows.Forms.Label redOutput;
+        private System.Windows.Forms.Label blueOutput;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label back;
+        private System.Windows.Forms.PictureBox sceneImage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ AdventureGame/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdventureGame
+{
+    public partial class Form1 : Form
+    {
+        int scene = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            output.Text = "you want a sandwich. you begin to make your sandwich but you realize you have no cheese. will you get cheese or settle for something else? ";
+            redOutput.Text = "sandwich";
+            blueOutput.Text = "something else";
+
+            sceneImage.BackgroundImage = Properties.Resources.sceneOne;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+           if (e.KeyCode == Keys.M) // red button 
+            {
+                if (scene == 0) { scene = 1; }
+                else if (scene == 1) { scene = 2; }
+                else if (scene == 2) { scene = 0; }
+                else if (scene == 3) { scene = 4; }
+                else if (scene == 4) { scene = 5; }
+                else if (scene == 5) { scene = 6; }
+                else if (scene == 6) { scene = 0; }
+                else if (scene == 7) { scene = 9; }
+                else if (scene == 8) { scene = 0; }
+                else if (scene == 9) { scene = 10; }
+                else if (scene == 10) { scene = 99; }
+                else if (scene == 11) { scene = 12; }
+                else if (scene == 12) { scene = 13; }
+                else if (scene == 13) { scene = 0;  }
+                else if (scene == 14) { scene = 99; }
+                else if (scene == 15) { scene = 14; }
+                else if (scene == 16) { scene = 0; }
+                else if (scene == 17) { scene = 0; }
+               
+            }
+           if (e.KeyCode == Keys.B)  //blue button press
+            {
+                if (scene == 0) { scene = 3; }
+                else if (scene == 1) { scene = 3; }
+                else if (scene == 2) { scene = 99; }
+                else if (scene == 3) { scene = 11; }
+                else if (scene == 4) { scene = 11; }
+                else if (scene == 5) { scene = 7; }
+                else if (scene == 6) { scene = 99; }
+                else if (scene == 7) { scene = 8; }
+                else if (scene == 8) { scene = 99; }
+                else if (scene == 9) { scene = 17; }
+                else if (scene == 10) { scene = 0; }
+                else if (scene == 11) { scene = 14; }
+                else if (scene == 12) { scene = 15; }
+                else if (scene == 13) { scene = 99; }
+                else if (scene == 14) { scene = 0; }
+                else if (scene == 15) { scene = 16; }
+                else if (scene == 16) { scene = 99; }
+                else if (scene == 17) { scene = 99; }
+            }
+
+           switch (scene)
+            {
+                case 0:
+                    break;
+                case 1:
+                    output.Text = "you monster, you can not just settle for something else  you want a sandwich like a real man. look for cheese or not";
+
+                    redOutput.Text = "look for cheese";
+                    blueOutput.Text = " something else";
+                    break;
+                case 2:
+                    output.Text = "you decide not to have a sandwich because you are a weak little man baby. the end. would you like to play again? ";
+                    redOutput.Text = "yes";
+                    blueOutput.Text = "no";
+                    break;
+                case 3:
+                    output.Text = "you decide that cheese must be found in order to enjoy your sandwich, but where do you start. go outside or look in the fridge again ? ";
+                    redOutput.Text = "look in fridge";
+                    blueOutput.Text = "go outside";
+                    break;
+                case 4:
+                    output.Text = "you open the fridge and still no cheese is their. you decide that there are only two options from this point,  go outside to fine cheese or open a portal to hell.";
+                    redOutput.Text = "open portal";
+                        blueOutput.Text = "go outside";
+                    sceneImage.BackgroundImage = Properties.Resources.scene4;
+                    break;
+                case 5:
+                    output.Text = "you figer that goats are associated with hell so you can get some goat cheese to shake things up. oh but how as that portal made again? yeet milk at the wall or yeet milk on the floor?";
+                    redOutput.Text = "floor";
+                        blueOutput.Text = "wall";
+                    sceneImage.BackgroundImage = Properties.Resources.scene5;
+                    break;
+                case 6:
+                    output.Text = "well now you just have a bunch of milk on the floor. you can no longer have you sandwich as now you have to clean this big mess, good job buddy. play again?";
+                    redOutput.Text = "yes";
+                    blueOutput.Text = "no";
+                    sceneImage.BackgroundImage = Properties.Resources.scene6;
+                    break;
+                case 7:
+                    output.Text = "after yeeting milk at the wall a portal to hell opens up.  will you walk in or wright a note and pass it through?";
+                    redOutput.Text = "pass note";
+                    blueOutput.Text = "go on in";
+                    break;
+                case 8:
+                    output.Text = "you walk in and the portal closes behind you. you are trapped in hell because you were to lazy to go to the store and get some bloody cheese. play again?";
+                    redOutput.Text = "yes";
+                    blueOutput.Text = "no";
+                    break;
+                case 9:
+                    output.Text = "you pass a note through and after about a minute to so a block of cheese is tossed out. use it? ";
+                    redOutput.Text = "Yes";
+                    blueOutput.Text = "No";
+                    sceneImage.BackgroundImage = Properties.Resources.scene9;
+                    break;
+                case 10:
+                    output.Text = "you place the cheese upon your sandwich and as you eat it you realize something, its hot like really hot. you're just on fire, but its worth it. play again? ";
+                    redOutput.Text = " yes";
+                    blueOutput.Text = "no";
+                    break;
+                case 11:
+                    output.Text = "you go outside. great you're outside now, so now what? will you go to the store or  go to a farm?";
+                    redOutput.Text = "farm";
+                    blueOutput.Text = "store";
+                    sceneImage.BackgroundImage = Properties.Resources.scene11;
+                    break;
+                case 12:
+                    output.Text = "you go to your local farm and you see it, a cow. will you obtain its milk?";
+                    redOutput.Text = "yes";
+                    blueOutput.Text = "no";
+                    sceneImage.BackgroundImage = Properties.Resources.scene12;
+                    break;
+                case 13:
+                    output.Text = "you go up to the cow  but as you go to milk it you realize that this is not a cow, it is a bull. it kicks you in the face and now you have brain damage. play again? ";
+                    redOutput.Text = "yes";
+                    blueOutput.Text = "no";
+                    sceneImage.BackgroundImage = Properties.Resources.scene13;
+                    break;
+                case 14:
+                    output.Text = "you go to the store and get some cheese. you can finally enjoy your sandwich. the end. would you like to play again? ";
+                    redOutput.Text = "yes";
+                    blueOutput.Text = "no";
+                    sceneImage.BackgroundImage = Properties.Resources.scene14;
+                    break;
+                case 15:
+                    output.Text = "you remember that you dont know how to make cheese. go to the store?";
+                    redOutput.Text = "yes";
+                    blueOutput.Text = "no";
+                    break;
+                case 16:
+                    output.Text = "well now you have no cheese and cannot enjoy your sandwich. play again?";
+                    redOutput.Text = "yes";
+                    blueOutput.Text = "no";
+                    break;
+                case 17:
+                    output.Text = "you decide not to use it and now you have no cheese for you sandwich. play again?";
+                    redOutput.Text = "yes";
+                    blueOutput.Text = "no";
+                    break;
+                case 99:
+                    output.Text = " you have no cheese for your sandwich. you have failed play again?";
+                    redOutput.Text = "yes";
+                    blueOutput.Text = "no";
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+          
+          
+        }
+
+    }
+}
